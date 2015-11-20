cb1239d96cd9eff8b2352e2a1650198031101b8f
Rush.D.Xzj <xzjxylophone@gmail.com>
2015-11-20 10:22:33 +0800

    Add

---
+++ RXActionSheetView.podspec
@@ -0,0 +1,22 @@
+
+Pod::Spec.new do |s|
+  s.name     = "RXActionSheetView"
+  s.version  = "0.1"
+  s.license  = "MIT"
+  s.summary  = "RXActionSheetView is a base as view"
+  s.homepage = "https://github.com/xzjxylophone/RXActionSheetView"
+  s.author   = { 'Rush.D.Xzj' => 'xzjxylophoe@gmail.com' }
+  s.social_media_url = "http://weibo.com/xzjxylophone"
+  s.source   = { :git => 'https://github.com/xzjxylophone/RXActionSheetView.git', :tag => "v#{s.version}" }
+  s.description = %{
+        RXCheckView is a base as view.
+  }
+  s.source_files = 'RXActionSheetView/*.{h,m}'
+  s.frameworks = 'Foundation', 'UIKit'
+  s.requires_arc = true
+  s.platform = :ios, '5.0'
+
+end
+
+
+
---
+++ RXActionSheetView/RXActionSheetView.m
@@ -0,0 +1,129 @@
+//
+//  RXActionSheetView.m
+//  YYTS
+//
+//  Created by Rush.D.Xzj on 15/9/24.
+//  Copyright (c) 2015年 Rush.D.Xzj. All rights reserved.
+//
+
+#import "RXActionSheetView.h"
+
+@interface RXActionSheetView ()
+
+
+@property (nonatomic, strong) UITapGestureRecognizer *tgr;
+
+
+@end
+
+
+
+@implementation RXActionSheetView
+
+
+- (UITapGestureRecognizer *)tgr
+{
+    if (_tgr == nil) {
+        _tgr = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(vBgAction:)];
+    }
+    return _tgr;
+}
+
+- (void)vBgAction:(id)sender
+{
+    [self close];
+}
+
+- (void)setIsSupportClickOtherToClose:(BOOL)isSupportClickOtherToClose
+{
+    _isSupportClickOtherToClose = isSupportClickOtherToClose;
+    if (isSupportClickOtherToClose) {
+        self.vBg.userInteractionEnabled = YES;
+        [self.vBg addGestureRecognizer:self.tgr];
+    } else {
+        [self.vBg removeGestureRecognizer:self.tgr];
+    }
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        CGFloat width = [UIScreen mainScreen].bounds.size.width;
+        self.vBg = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, [UIScreen mainScreen].bounds.size.height)];
+        self.vBg.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
+        self.isSupportClickOtherToClose = YES;
+        self.backgroundColor = [UIColor clearColor];
+        
+        
+        
+    }
+    return self;
+}
+
+- (void)setFrameTop:(CGFloat)top
+{
+    CGRect frame = self.frame;
+    frame.origin.y = top;
+    self.frame = frame;
+}
+
+
+
+- (CGFloat)showY
+{
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    return (height - self.frame.size.height) / 2.0f;
+}
+
+
+- (void)show
+{
+    [self.vBg addSubview:self];
+    UIView *window = [UIApplication sharedApplication].keyWindow;
+    [window addSubview:self.vBg];
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    CGFloat selfY = self.showY;
+    [self setFrameTop:height];
+    if (self.isSupportAnimate) {
+        [UIView beginAnimations:@"abc" context:nil];
+        [UIView setAnimationDuration:0.5];
+    }
+    [self setFrameTop:selfY];
+    if (self.isSupportAnimate) {
+        [UIView commitAnimations];
+    }
+    
+}
+- (void)close
+{
+    CGFloat height = [UIScreen mainScreen].bounds.size.height;
+    if (self.isSupportAnimate) {
+        [UIView beginAnimations:@"stop" context:nil];
+        [UIView setAnimationDuration:0.5];
+        [UIView setAnimationDelegate:self];
+        [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:)];
+    } else {
+        [self.vBg removeFromSuperview];
+        [self removeFromSuperview];
+    }
+    [self setFrameTop:height];
+    if (self.isSupportAnimate) {
+        [UIView commitAnimations];
+    }
+}
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    
+    [self.vBg removeFromSuperview];
+    [self removeFromSuperview];
+}
+
+
++ (id)rxActionSheetView
+{
+    Class cls = [self class];
+    id result = [[cls alloc] initWithFrame:CGRectZero];
+    return result;
+}
+
+@end
